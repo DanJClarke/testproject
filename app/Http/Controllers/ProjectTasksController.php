7085138b26e31016073cc0fd224d1dcb0f764561
Daniel Clarke <daniel.clarke@Daniels-Mac-mini.local>
2019-11-25 15:03:47 +0000

    refactor the code to be more restful, creating a dedicated controller for the completed task.

--- app/Http/Controllers/ProjectTasksController.php
+++ app/Http/Controllers/ProjectTasksController.php
@@ -28,13 +28,47 @@ class ProjectTasksController extends Controller
         return back();
     }
 
+    /* This has been totally superseeded by a new controller to handel the completed state.
+
     public function update(Task $task)
     {
+
+
+        $method = request()->has('completed') ? 'complete' : 'incomplete';
+
+        $task->$method();
+
+        /*
+
+
+        //=============VERSON 4 ==================
+
+        request()->has('completed') ? $task->complete() : $task->incomplete();
+        }
+
+        //=============VERSON 3 ==================
+
+        if(request()->has('completed')){
+            $task->complete();
+        }else{
+            $task->incomplete();
+        }
+
+        //=============VERSON 2 ==================
+
+        $task->complete(request()->has('completed'));
+
+        /*
+
+        //=============VERSON 1 ==================
         $task->update([
             'completed' => request()->has('completed')
         ]);
 
+
         return back();
     }
+    */
+
 
 }
--- routes/web.php
+++ routes/web.php
@@ -105,3 +105,7 @@ Route::resource('projects', 'ProjectsController');
 Route::post('/projects/{project}/tasks', 'ProjectTasksController@store');
 
 Route::patch('/tasks/{task}', 'ProjectTasksController@update');
+
+Route::post('/completed-tasks/{task}', 'CompletedTasksController@store');
+
+Route::delete('/completed-tasks/{task}', 'CompletedTasksController@destroy');
